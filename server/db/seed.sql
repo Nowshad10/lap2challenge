dd43acdc38128e06c0998fec32dc24980114889b
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-06 10:53:15 +0100

    commit

--- server/db/seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO posts (title, author, body)
-VALUES
-    ('My Secret', 'Stuart Little', 'This is my first secret story'),
-    ('I love buildings', 'Ikenna', 'I like it when buildings are curvy and round'),
-    ('Rubber Duckies', 'Sami', "I can't have baths without a rubber duck, it doesnt feel good.");
--- server/db/setup.sql
+++ server/db/setup.sql
@@ -6,3 +6,9 @@ CREATE TABLE posts (
     author VARCHAR NOT NULL,
     body VARCHAR NOT NULL
 );
+
+INSERT INTO posts (title, author, body)
+VALUES
+    ('My Secret', 'Stuart Little', 'This is my first secret story'),
+    ('I love buildings', 'Ikenna', 'I like it when buildings are curvy and round'),
+    ('Rubber Duckies', 'Sami', "I can't have baths without a rubber duck, it doesnt feel good.");

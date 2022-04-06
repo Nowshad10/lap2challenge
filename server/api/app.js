60c88f68d502c9c3c601b789ff91a6cde4bb3188
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-06 12:01:35 +0100

    all backend routes working

--- server/api/app.js
+++ server/api/app.js
@@ -19,7 +19,25 @@ app.get('/posts', async (req, res) => {
         const posts = await Post.all
         res.status(200).json({posts})
     } catch (err) {
-        res.status(500).json(err)
+        res.status(422).json(err)
+    }
+})
+
+app.get('/posts/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(parseInt(req.params.id))
+        res.status(200).json(post)
+    } catch (err) {
+       res.status(500).json(err) 
+    }
+})
+
+app.post('/posts', async (req, res) => {
+    try {
+        const post = await Post.create(req.body)
+        res.status(201).json(post)
+    } catch (err) {
+        res.status(422).json(err)
     }
 })
 
--- server/api/models/Post.js
+++ server/api/models/Post.js
@@ -19,6 +19,30 @@ class Post {
             }
         })
     }
+
+    static findById(id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let getPost = await db.query(`SELECT * FROM posts WHERE posts.id = $1;`, [ id ])
+                let post = new Post(getPost.rows[0])
+                resolve(post)
+            } catch (err) {
+                reject(`Post not found!`)
+            }
+        })
+    }
+
+    static create(data) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let insertPost = await db.query(`INSERT INTO posts (title, author, body) VALUES ($1, $2, $3) RETURNING *;`, [ data.title, data.author, data.body ])
+                let createPost = new Post(insertPost.rows[0])
+                resolve(createPost)
+            } catch (err) {
+                reject('Cannot create post!')
+            }
+        })
+    }
 }
 
 module.exports = Post
